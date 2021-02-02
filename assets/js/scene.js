98011d844414f9e142b6bcc110ba2559c3ae4643
NeoxAzrot1 <sami.lepays@gmail.com>
2021-02-02 11:37:21 +0100

    prototype scene

---
+++ assets/js/scene.js
@@ -0,0 +1,70 @@
+// Variable pour changer de scènes
+const scenes = [
+  {
+    x: 0,
+    y: 0,
+    scale: 1
+  },
+  {
+    x: -600,
+    y: 200,
+    scale: 2
+  },
+  {
+    x: -1600,
+    y: -1200,
+    scale: 1.5
+  }
+]
+
+// Fonction pour changer de scène
+let sceneIndex
+
+const moveToScene = (sceneIndex) => {
+  gsap.timeline().to('.container', {
+    duration: 3,
+    x: scenes[sceneIndex].x,
+    y: scenes[sceneIndex].y,
+    scale: scenes[sceneIndex].scale
+  })
+
+  showArrow()
+}
+
+// Fonctions pour les flèches
+const leftArrow = $('#previous')
+const rightArrow = $('#next')
+
+leftArrow.click(() => {
+  if(sceneIndex != 0) sceneIndex--
+  
+  moveToScene(sceneIndex)
+})
+
+rightArrow.click(() => {
+  if(sceneIndex != 2) sceneIndex++
+  
+  moveToScene(sceneIndex)
+})
+
+const showArrow = () => {
+  if(sceneIndex == 0) {
+    leftArrow.fadeOut()
+  } else {
+    leftArrow.fadeIn()
+  }
+  
+  if(sceneIndex == 2) {
+    rightArrow.fadeOut()
+  } else {
+    rightArrow.fadeIn()
+  }
+}
+
+// Fonction d'initialisation
+const init = () => {
+  sceneIndex = 0
+  moveToScene(sceneIndex)
+}
+
+init()
